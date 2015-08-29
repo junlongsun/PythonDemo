ae19d9d775349e0bb699f00a563dd4c358bddfa3
Junlong Sun <junlong.sun@colorado.edu>
2015-08-29 13:00:37 -0600

    initial

---
+++ using_name.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# Filename: using_name.py
+
+if __name__ == '__main__':
+    print 'This program is being run by itself'
+else:
+    print 'I am being imported from another module'
+
+
+#$ python using_name.py
+#This program is being run by itself
+
+#$ python
+#>>> import using_name
+#I am being imported from another module
